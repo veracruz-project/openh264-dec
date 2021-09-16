02a4b2ae18dff3de1e4ea038d89a169b4fa17eba
Guilhem Bryant <Guilhem.Bryant@arm.com>
2021-09-16 11:36:43 +0100

    Add licensing and authorship headers

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OpenH264 decoder
 
-Library built on top of [`openh264`](https://github.com/cisco/openh264), providing a higher level API to easily decode H264
+Library built on top of [`openh264`](https://github.com/cisco/openh264), providing a higher level API to easily decode H.264
--- inc/h264dec.h
+++ inc/h264dec.h
@@ -1,3 +1,17 @@
+/*
+H.264 decoding library, based on the OpenH264 codec library
+
+AUTHORS
+
+The Veracruz Development Team.
+
+COPYRIGHT AND LICENSING
+
+See the `LICENSE_MIT.markdown` file in the root directory for copyright and
+licensing information.
+Based on the OpenH264 codec library, BSD license (https://github.com/cisco/openh264)
+*/
+
 #include <string>
 
 int32_t h264_decode(std::string strInputFile, std::string strOutputFile);
